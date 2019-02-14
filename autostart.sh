c6b1a63b3f1546f5a6bec56af71462ee5b5ec387
darekem73 <darek.mastalerz@gmail.com>
2019-02-14 09:14:57 +0100

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -2,10 +2,14 @@ nitrogen --restore &
 conky -d -c ~/.conkyrc-stat
 nm-applet &
 blueman-applet &
+pamac-tray &
 system-config-printer-applet &
+volumeicon &
+xfce4-power-manager &
 unclutter -idle 1 -root &
 mopidy &
 compton &
 setxkbmap -option caps:escape
 slstatus &
-synclient TapButton2=3 TapButton3=2
+#synclient TapButton2=3 TapButton3=2
+xautolock -time 10 -locker screenlock
