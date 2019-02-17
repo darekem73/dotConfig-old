07dfcbb139ed4ef7a3cb59e1b0e2172a93162844
darekem73 <darek.mastalerz@gmail.com>
2019-02-17 10:43:57 +0100

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -10,6 +10,7 @@ unclutter -idle 1 -root &
 mopidy &
 compton &
 setxkbmap -option caps:escape
+xset r rate 200 40
 slstatus &
 #synclient TapButton2=3 TapButton3=2
 xautolock -time 10 -locker screenlock
