50f3ad140cf6980198ed6444dd2fd6c021ec9501
Dariusz Mastalerz <darek.mastalerz@gmail.com>
2019-05-27 13:01:29 +0200

    FreeBSD initial config

--- autostart.sh
+++ autostart.sh
@@ -5,12 +5,13 @@ conky -d -c ~/.conkyrc-stat
 #pamac-tray &
 #system-config-printer-applet &
 #volumeicon &
-xfce4-power-manager &
+#xfce4-power-manager &
 #unclutter -idle 1 -root &
 #mopidy &
 compton &
 setxkbmap -option caps:escape
 xset r rate 200 40
-slstatus &
+#slstatus &
+while true; do xsetroot -name "`date '+%Y-%m-%d %H:%M'1`"; sleep 10; done &
 #synclient TapButton2=3 TapButton3=2
-xautolock -time 10 -locker screenlock
+#xautolock -time 10 -locker screenlock
